6225a52309567ea7eb9dc135f77b2e90e5431cca
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-05-06 15:05:39 +0200

    Completo gli ultimi punti dell'esercizio

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,18 +22,30 @@ alert("I numeri estratti sono: " + numeriEstratti + ". Memorizzali!");
 
 //Da li parte un timer di 30 secondi.
 var tempo = 5; //31
+var numeriUtente = [];
+
 var intervallo = setInterval(function () {
     tempo--;
     document.getElementById("timer").innerHTML = tempo;
     if (tempo == 0) { //sistemare! parte prima l'allerta e poi si stampa a video lo 0
         clearInterval(intervallo);
-        alert("fine");
+
+
+        //Dopo 30 secondi l'utente deve inserire, uno alla volta,
+        //i numeri che ha visto precedentemente, tramite il prompt().
+        //Dopo che sono stati inseriti i 5 numeri, il software dice
+        //quanti e quali dei numeri da indovinare sono stati individuati.
+        for (var i = 0; i < 5; i++){
+            do {
+                var numeroInserito = parseInt(prompt(`Inserisci il ${i + 1} numero!`));
+            } while (isNaN(numeroInserito));
+            if (numeriEstratti.includes(numeroInserito)) {
+                numeriUtente.push(numeroInserito);
+            }
+        }
+        alert(`Ne hai indovinati ${numeriUtente.length} su ${numeriEstratti.length}! Cioè hai indovinato: ${numeriUtente}.`);
     }
 }, 1000);
 
 
-//Dopo 30 secondi l'utente deve inserire, uno alla volta,
-//i numeri che ha visto precedentemente, tramite il prompt().
 
-//Dopo che sono stati inseriti i 5 numeri, il software dice 
-//quanti e quali dei numeri da indovinare sono stati individuati.
